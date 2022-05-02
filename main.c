ee3b0e0a6aef515ec4f69f7043ec1a40d0eb0acc
egoros67 <egoros678@yandex.ru>
2022-05-02 13:36:11 +0300

    first problems with input

--- main.c
+++ main.c
@@ -1,33 +1,33 @@
+#define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
 #include<strings.h>
 #include<stdio.h>
 #include<stdlib.h>
-#define p(i,j) idata[(i)+(j)*iw]
 int main(){
-	char* inputPath = "/Pictures/input.png";
+	char* inputPath ="hampster.png";
 	int iw,ih,n;
 	unsigned char * idata = stbi_load(inputPath,&iw,&ih,&n,0);
 	if (idata == NULL){
-		printf("ERROR: can't read file %s/n",inputPath);
+		printf("ERROR: can't read file %s\n",inputPath);
 		return 1;
 }
-	char *pixel =idata;
-	pixel=pixel +n;
-	char*pixel=p(i,j);
-	unsigned char* odata(unsigned char *) malloc(iw*ih*n)
+/*	char *pixel =idata;
+	unsigned char* odata(unsigned char *) malloc(iw*ih*n);
 	int d=3;
 	for(int s=-d;s<=d;s++){
 		for(int t=-d;t<=d;t++){
 			p1=p(i+s,j+t);
 			if (abs p1[0]-p[0])<20){
-				...
+				
 }
 }
 }
-char *outputPath="~/Pictures/output.png";
-stbi_write_png(outputPath,ow,oh,n,odata,0);
+char *outputPath="output.png";
+stbi_write_png(outputPath,ow,oh,n,odata,0);*/
 stbi_image_free(idata);
-stbi_image_free(odata);
+//stbi_image_free(odata);
+printf("Loaded image with a width of %dpx,a height of %dpx and %d channels\n",iw,ih,n);
 return 0;
 }
