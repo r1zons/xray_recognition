295c4bda54151cec31719a5b75699384f564a187
egoros67 <egoros678@yandex.ru>
2022-05-03 17:04:59 +0300

    second steps

--- main.c
+++ main.c
@@ -7,19 +7,23 @@
 #include<stdlib.h>
 int main(){
 	char* inputPath ="hampster.png";
-	int iw,ih,n;
+	int iw,ih,n,c=0;
+	int** graph=malloc(iw*ih*n/4*iw*ih*n/4*(sizeof(int));	
 	unsigned char * idata = stbi_load(inputPath,&iw,&ih,&n,0);
 	if (idata == NULL){
 		printf("ERROR: can't read file \n");
-		return 1;
-}
-/*	char *pixel =idata;
+		return 1; 
+	char *pixel =idata;
 	unsigned char* odata(unsigned char *) malloc(iw*ih*n);
+	for(int i=0;i<iw*ih*n;i+=5){
+		odata[c]=(pixel[i]*11+pixel[i+1]*16+5*pixel[i+2])/32;	
+		c+=1;}
 	int d=3;
-	for(int s=-d;s<=d;s++){
-		for(int t=-d;t<=d;t++){
-			p1=p(i+s,j+t);
-			if (abs p1[0]-p[0])<20){
+	for(int s=0;s<=iw*ih*n/4;s++){
+		for(int t=s;t<=iw*ih*n/4;t++){
+			
+			if (abs(odata[s]-odata[t])<20)){
+				graph[s][t]=abs(odata[s]-odata[t]);
 				
 }
 }
@@ -27,7 +31,7 @@ int main(){
 char *outputPath="output.png";
 stbi_write_png(outputPath,ow,oh,n,odata,0);*/
 stbi_image_free(idata);
-//stbi_image_free(odata);
+stbi_image_free(odata);
 printf("Loaded image with a width of %dpx,a height of %dpx and %d channels\n",iw,ih,n);
 return 0;
 }
