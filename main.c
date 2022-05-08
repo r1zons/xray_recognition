69df20d136e031d97884246d1b304fb994b14679
r1zons <axelstriker86@gmail.com>
2022-05-08 12:55:58 +0300

    some codestyle

--- main.c
+++ main.c
@@ -6,22 +6,29 @@
 #include<strings.h>
 #include<stdio.h>
 #include<stdlib.h>
-int main(){
-	char* inputPath ="hampster.png";
-	int iw,ih,n,c=0;
-	unsigned char * idata = stbi_load(inputPath,&iw,&ih,&n,0);
-	int** graph =malloc(iw*ih*iw*ih*(sizeof(int)));
-	if (idata == NULL){
+
+
+int main() {
+	char *inputPath = "hampster.png";
+	int InputWidth, InputHeight, n;
+	unsigned char *idata = stbi_load(inputPath, &InputWidth, &InputHeight, &n,0);
+
+	if (!idata) {
 		printf("ERROR: can't read file \n");
-		return 1;} 
-	char *pixel =idata;
-	unsigned char* odata=malloc(iw*ih*(sizeof(unsigned char)));
-	for(int i=0;i<iw*ih*n;i+=4){
-		odata[c]=(pixel[i]*11+pixel[i+1]*16+5*pixel[i+2])/32;	
-		c+=1;}
+		return 1;
+	}
+
+	printf("Loaded image with a width of %dpx,a height of %dpx and %d channels\n",InputWidth,InputHeight,n);
+	
+	char *pixel = idata;
+	unsigned char* odata = malloc(InputWidth * InputHeight * sizeof(unsigned char));
+	for (int i = 0; i < InputWidth * InputHeight * n; i += 4) {
+		odata[i / 4] = (pixel[i] * 11 + pixel[i + 1] * 16 + 5 * pixel[i + 2]) / 32;	
+	}
+	
 	/*int d=3;
-	for(int s=0;s<=iw*ih*n;s++){
-		for(int t=s;t<=iw*ih;t++){
+	for(int s=0;s<=InputWidth*InputHeight*n;s++){
+		for(int t=s;t<=InputWidth*InputHeight;t++){
 			
 			if (abs(odata[s]-odata[t])<20)){
 				graph[s][t]=abs(odata[s]-odata[t]);
@@ -29,10 +36,9 @@ int main(){
 }
 }
 }	*/
-char *outputPath="output.png";
-stbi_write_png(outputPath,iw,ih,1,odata,0);
+char *outputPath = "output.png";
+stbi_write_png(outputPath, InputWidth, InputHeight, 1, odata, 0);
 stbi_image_free(idata);
 stbi_image_free(odata);
-printf("Loaded image with a width of %dpx,a height of %dpx and %d channels\n",iw,ih,n);
 return 0;
 }
